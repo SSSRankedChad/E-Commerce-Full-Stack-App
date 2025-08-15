e80714d11b2ce99e742b82a270dced83a0a0e754
Halim Muhammad <hm636848@gmail.com>
2025-08-15 12:34:19 -0700

    added Carousel to Header

--- frontend/src/src/components/Header/header.js
+++ frontend/src/src/components/Header/header.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-3d-carousel';
+import 'react-responsive-3d-carousel/dist/styles.css';
 import './header.css';
-import Navbar from '../Navbar/navbar.js';
 import img1 from '../../public/resources/gold-zipper-on-black-fashion-backpack.jpg';
 import img2 from '../../public/resources/black-bag-over-the-shoulder.jpg';
 import img3 from '../../public/resources/stacked-bracelets.jpg';
 import img4 from '../../public/resources/DIY-beard-balm.jpg';
 import img5 from '../../public/resources/blue-t-shirt.jpg';
 
+const images = [
+  <img src={img1} />,
+  <img src={img2} />,
+  <img src={img3} />,
+  <img src={img4} />,
+  <img src={img5} />,
+];
+
+
+
+
 const Header = () => {
   return (
     <>
     <h1> Welcome! </h1>
     <div className="header">
-      <img src={img1} />
-      <img src={img2} />
-      <img src={img3} />
-      <img src={img4} />
-      <img src={img5} />
+      <Carousel
+      items={images}
+      startIndex={0}
+      autoPlay
+      currentIndex={(currentIndex) => console.log(currentIndex)} />
     </div>
   </>
   )
