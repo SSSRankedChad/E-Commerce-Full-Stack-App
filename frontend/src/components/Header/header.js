4e2217eb450452d1f78c3f5de51e68cc999ee0a6
Halim Muhammad <hm636848@gmail.com>
2025-08-30 21:04:07 -0700

    added swiper

--- frontend/src/components/Header/header.js
+++ frontend/src/components/Header/header.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-3d-carousel';
-import 'react-responsive-3d-carousel/dist/styles.css';
+import SlideShow from '../../components/Swiper/swiper.js';
 import './header.css';
-import img1 from '../../public/resources/gold-zipper-on-black-fashion-backpack.jpg';
-import img2 from '../../public/resources/black-bag-over-the-shoulder.jpg';
-import img3 from '../../public/resources/stacked-bracelets.jpg';
-import img4 from '../../public/resources/DIY-beard-balm.jpg';
-import img5 from '../../public/resources/blue-t-shirt.jpg';
-
-const images = [
-  <img src={img1} />,
-  <img src={img2} />,
-  <img src={img3} />,
-  <img src={img4} />,
-  <img src={img5} />,
-];
 
 
 
@@ -24,14 +10,7 @@ const Header = () => {
     <>
     <h1> Welcome! </h1>
     <div className="header">
-      <Carousel
-      items={images}
-      startIndex={0}
-      autoPlay
-      showArrows={false}
-      showIndicators={false}
-      showStatus={false}
-      infiniteLoop={true} />
+     <SlideShow />
     </div>
   </>
   )
