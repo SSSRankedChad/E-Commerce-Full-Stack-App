3831c39e752b6b3ff2a7f6c69864fc2209d4a292
Halim Muhammad <hm636848@gmail.com>
2025-09-27 12:02:35 -0700

    updated components/product

--- frontend/src/components/Product/product.js
+++ frontend/src/components/Product/product.js
@@ -5,20 +5,21 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
-import { setProductId } from '../../store/Product/productSlice.js';
+import { setProductId, selectProducts, selectFilteredProducts, selectProduct } from '../../store/Product/productSlice.js';
 import { selectUserId } from '../../store/User/userSlice';
 import { selectCartId, loadCart, updateCart } from '../../store/Cart/cartSlice';
 import { AddCircle } from '@mui/icons-material';
 import { RemoveCircle } from '@mui/icons-material';
 
 
-const Product = ({product, display}) => {
+const Product = () => {
 
   const [cartQuantity, setCartQuantity] = useState(product?.cart_quantity);
   const [quantity, setQuantity] = useState(product?.cart_quantity);
+  const products = useSelector(selectProducts);
   const cartId = useSelector(selectCartId);
   const userId = useSelector(selectUserId);
-  const productId = product?.product_id;
+  const product = product?.product_id;
   const dispatch = useDispatch();
   let navigate = useNavigate();
 
@@ -58,7 +59,7 @@ const Product = ({product, display}) => {
     }
   }, [cartId, userId, productId, cartQuantity, quantity, dispatch]);
 
-  if(!display || !product) {
+  if(!product) {
     return (
       <div className="empty__product__container">
        <Loader />
@@ -72,94 +73,32 @@ const Product = ({product, display}) => {
   }
 
 
-  if(display === 'inCart') {
-     return (
-    <section className="Product__inCart">
-                <img className="Product__inCart__image" src={product?.url} alt="" />
-                <div className="Product__inCart__info">
-                    <div className="Product__inCart__container">
-                        <p className="Product__inCart__label">PRODUCT NAME</p>
-                        <Link to={`/products/${productId}`}>
-                            <h2 id={product?.product_id} className="Product__inCart__name" onClick={handleProductClick}>{product?.name}</h2>
-                        </Link>
-                    </div>
-                    <div className="Product__inCart__container">
-                        <p className="Product__inCart__label">QUANTITY</p>
-                        <div className="Product__inCart__quantity__container">
-                            <IconButton onClick={handleRemoveClick}><RemoveCircle/></IconButton>
-                            <input id="quantity" className="Product__inCart__quantity" type="number" name="quantity" min="0" max="100"
-                                value={cartQuantity} readOnly/>
-                            <IconButton onClick={handleAddClick}><AddCircle/></IconButton>
-                        </div>
-                    </div>
-                    <div className="Product__inCart__container">
-                        <p className="Product__inCart__label">PRICE</p>
-                        <p className="Product__inCart__price">{product?.sell_price}</p>
-                    </div>
-                    <div className="Product__inCart__container">
-                        <p className="Product__inCart__label">ITEM TOTAL</p>
-                        <p className="Product__inCart__itemTotal">${product?.item_total}</p>
-                    </div>
-                </div>
-    </section>
-    );
-}
-
-
-else if(display === 'detail') {
-     return (
-     <section className="Product__details">
-                <div className="Product__details__main">
-                    <img className="Product__details__image" src={product?.url} alt="" />
-                    <div className="Product__details__main__info">
-                        <div className="Product__details__main__container">
-                            <p className="Product__details__label">PRODUCT NAME</p>
-                            <h2 id={product?.product_id} className="Product__details__name" onClick={handleProductClick}>{product?.name}</h2>
-                        </div>
-                        <div className="Product__details__main__container">
-                            <p className="Product__details__label">CATEGORY</p>
-                            <p className="Product__details__category">{product?.category}</p>
-                        </div>
-                        <div className="Product__details__main__container">
-                            <p className="Product__details__label">PRICE</p>
-                            <p className="Product__details__price">{product?.sell_price}</p>
-                        </div>
-                        <Button name="Add to Cart" onClick={handleCartClick}/>
-                    </div>
-                </div>
-                <div className="Product__details__container">
-                    <div className="Product__details__info">
-                        <div className="Product__details__label__container">
-                            <p className="Product__details__label">IN STOCK</p>
-                            <p className="Product__details__stock">{product?.stock_quantity}</p>
-                        </div>
-                        <div className="Product__details__label__container">
-                            <p className="Product__details__label">DESCRIPTION</p>
-                            <p className="Product__details__description">{product?.description}</p>
-                        </div>
-                        <div className="Product__details__label__container">
-                            <p className="Product__details__label">MANUFACTURER</p>
-                            <p className="Product__details__manufacturer">{product?.manufacturer}</p>
-                        </div>
-                    </div>
-                </div>
-       </section>
-     );
-}
-
   return (
-     <section className="Product">
-                <p className="Product__category">{product?.category}</p>
-                <img className="Product__image" src={product?.url} alt="" />
-                <div className="Product__info">
-                    <Link to={`/products/${productId}`}>
-                        <h2 id={product?.product_id} className="Product__name" onClick={handleProductClick}>{product?.name}</h2>
-                    </Link>
-                    <p className="Product__price">{product?.sell_price}</p>
-                    <Button name="Add to Cart" onClick={handleCartClick}/>
-                </div>
-      </section>
-  )
+    <div className="Product__container">
+      <div className="Product__name__container">
+       <h2 className="Product__name__header">{product.name}</h3>
+      </div>
+      <div className="Product__category__container">
+       <h3 className="Product__category__label"> Category: </h3>
+       <p className="Product__category__header"> {product.category} </p>
+      <div className="Product__image__container">
+       <img src={product.url}/>
+      </div>
+      <div className="Product__price__container">
+       <h3 className="Product__price__label"> Price: </h3>
+       <p className="Product__details__price"> {product.sell_price} </p>
+      </div>
+      <div className="Product__description__container">
+       <h3 className="Product__category__label"> Description: </h3>
+       <p className="Product__details__description">{product.description} </p>
+      </div>
+     <div className="Product__button_container">
+       <Button name="Add item" onClick={handleAddClick}></Button>
+       <Button name="Remove item" onClick={handleRemoveClick}></Button>
+       <Button name="Add to cart" onClick={handleCartClick}></Button>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
